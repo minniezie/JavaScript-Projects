c9602ed683bd8468f6229d757725bea53e191f4d
minniezie <minni.wadhwa9@gmail.com>
2025-01-26 16:19:15 -0500

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,46 @@
+alert("Hello, World!")
+
+// window.alert method
+window.alert("Hello, World!")
+
+document.write("Hello, World!")
+
+//document.write method
+var x = "Hello, World!"
+document.write(x)
+
+//creating two variables
+var Sent1 = "This is the begining of the string";
+var Sent2 = " and this is the end of the string.";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+document.write(A)
+
+//an expression
+document.write(33 + 84);
+
+window.alert(A)
+
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.")
+
+document.write("Be who you are and say what you feel," + "because those who mind don't matter and those who matter don't mind." + 
+    "-Dr. Seuss") 
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"
+document.write(Dad);
+
+document.write(5+5);
+
+fucntion myFirstFunction() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+function My_First_Function() {
+    var str = "This is the button text";
+    document.getElementById("Button_Text").innerHTML = str;
+}
